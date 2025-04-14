4766ee0d8645f76721c88c4b944cf8302b7b5992
Moktan Sujita <sujitamoktan77@gmail.com>
2025-04-14 21:20:14 +0545

    Jagged Array in Java

--- sujita_23_jagged_array/src/App.java
+++ sujita_23_jagged_array/src/App.java
@@ -1,5 +1,14 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        int [][] matrix=new int[][] {{1,2,3},{4,5,6,8},{9,10}};
+        for (int i = 0; i < matrix.length; i++) {
+            int sum=0;
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.println(matrix[i][j] +"");
+                sum+=matrix[i][j];
+            }
+            System.out.println("Sum of the row:" +sum);
+        }
     }
 }
