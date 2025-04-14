9970b226cbc0bc3417127820594a93611d551bda
Moktan Sujita <sujitamoktan77@gmail.com>
2025-04-14 21:43:41 +0545

    Jagged Array in Java

--- sujita_23_jagged_array/src/App.java
+++ sujita_23_jagged_array/src/App.java
@@ -1,7 +1,7 @@
 public class App {
 
     public static void main(String[] args) {
-        int [][] matrix=new int[][] {{1,2,3},{4,5,6,8},{9,10}};
+        int [][] matrix=new int[][] {{1,2,3},{4,5,6,8},{9,10}};//jagged array has non uniform length
         for (int i = 0; i < matrix.length; i++) {
             int sum=0;
             for (int j = 0; j < matrix[i].length; j++) {
