ea3a6fd732e67c7e547edf0f0df55de99f6c12dc
Moktan Sujita <sujitamoktan77@gmail.com>
2025-04-25 10:10:40 +0545

    Bitwise operations

---
+++ sujita_bitwise-opr_45/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+
+    public static void main(String[] args) {
+        int A=60;
+        int B=13;
+        System.out.println("A&B:" +(A&B));
+        System.out.println("A|B:" +(A|B));
+        System.out.println("A^B:" +(A^B));
+        System.out.println("~A:" +(~A));
+        System.out.println("A<<2:" +(A<<2));
+        System.out.println("A>>2:" +(A>>2));
+
+    }
+}
